47a421b778b4d5e76ca1ccccb4ed02999d6cde93
Agnieszka Szkurlat <javalodzdev@gmail.com>
2024-06-10 17:51:02 +0200

    Header component added

---
+++ app/meals/page.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+export default function MealsPage() {
+  return (
+    <main>
+      <h1>Meals</h1>
+    </main>
+  );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function SharedMealPage({ params }) {
+  return (
+    <main>
+      <h1>Shared</h1>
+    </main>
+  );
+}
