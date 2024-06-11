f7a2e45688966d71bd10a5a41560f1a0a1b995c8
Agnieszka Szkurlat <javalodzdev@gmail.com>
2024-06-11 19:38:13 +0200

    Meals-item and meals-grid  components added

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,25 @@
 import Link from "next/link";
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delecious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share You Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
